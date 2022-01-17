7ea362dfc235fdd106390b65b88c5e97f6570190
Pratik <bhagatpratik.2002@gmail.com>
2022-01-17 18:16:23 +0530

    mvc

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
-const Blog = require("./models/blog");
+const blogRoutes = require("./routes/blogRoutes");
 
 // express app
 const app = express();
@@ -19,7 +19,9 @@ mongoose
 // register view engine
 app.set("view engine", "ejs");
 
+// middleware
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
@@ -31,17 +33,7 @@ app.get("/about", (req, res) => {
 });
 
 // blog routes
-app.get("/blogs", (req, res) => {
-  Blog.find()
-    .sort({ createdAt: -1 })
-    .then((result) => {
-      res.render("index", { title: "All Blogs", blogs: result });
-    });
-});
-
-app.get("/blogs/create", (req, res) => {
-  res.render("create", { title: "Create" });
-});
+app.use("/blogs", blogRoutes);
 
 app.use((req, res) => {
   res.status(404).render("404", { title: "404" });
